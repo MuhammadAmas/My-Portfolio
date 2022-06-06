51f2e45d7587f4d1d291b07d8f1564df33969f96
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-06-06 18:43:01 +0500

    Updated

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const rootSelector = document.querySelector(':root');
 btnMode.addEventListener('click',()=>{
    rootSelector.style.setProperty('--dark','hsl(0 0%, 100%)');
    rootSelector.style.setProperty('--light','hsl(0 0%, 0%)');
-   rootSelector.style.setProperty('--semiDark','hsl(0 0%, 100%)');
+   rootSelector.style.setProperty('--semiDark','hsl(0, 2%, 76%)');
+   rootSelector.style.setProperty('--semiLight','hsl(0, 3%, 6%)');
 } )
