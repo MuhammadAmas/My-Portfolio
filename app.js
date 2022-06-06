61939985c2976210e367b1f87c98ce5552e7ed38
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-06-06 19:27:31 +0500

    Updated

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const rootSelector = document.querySelector(":root");
 let mode = "darkMode";
 btnMode.addEventListener("click", () => {
   if (mode === "darkMode") {
-    rootSelector.style.setProperty("--dark", "hsl(0 0%, 100%)");
-    rootSelector.style.setProperty("--light", "hsl(0 0%, 0%)");
+    rootSelector.style.setProperty("--dark", "hsl(0, 0%, 100%)");
+    rootSelector.style.setProperty("--light", "hsl(0, 0%, 0%)");
     rootSelector.style.setProperty("--semiDark", "hsl(0, 2%, 76%)");
     rootSelector.style.setProperty("--semiLight", "hsl(0, 3%, 6%)");
     mode = "lightMode";
-    console.log("light")
   } 
   else {
-    rootSelector.style.setProperty("--dark", "hsl(0 0%, 0%)");
-    rootSelector.style.setProperty("--light", "hsl(0 0%, 100%)");
+    rootSelector.style.setProperty("--dark", "hsl(0, 0%, 0%)");
+    rootSelector.style.setProperty("--light", "hsl(0, 0%, 100%)");
     rootSelector.style.setProperty("--semiDark", "hsl(0, 2%, 6%)");
     rootSelector.style.setProperty("--semiLight", "hsl(0, 3%, 76%)");
     mode = "darkMode";
-    console.log("dark")
   }
 });
