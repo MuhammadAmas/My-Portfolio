841683696261bdeeaf76d250f6dad01835f613f8
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-06-06 19:19:52 +0500

    Updated

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const btnMode = document.querySelector('.btn-mode');
-const rootSelector = document.querySelector(':root');
+const btnMode = document.querySelector(".btn-mode");
+const rootSelector = document.querySelector(":root");
 
-
-
-btnMode.addEventListener('click',()=>{
-   rootSelector.style.setProperty('--dark','hsl(0 0%, 100%)');
-   rootSelector.style.setProperty('--light','hsl(0 0%, 0%)');
-   rootSelector.style.setProperty('--semiDark','hsl(0, 2%, 76%)');
-   rootSelector.style.setProperty('--semiLight','hsl(0, 3%, 6%)');
-} )
+let mode = "dark";
+btnMode.addEventListener("click", () => {
+  if (mode === "dark") {
+    rootSelector.style.setProperty("--dark", "hsl(0 0%, 100%)");
+    rootSelector.style.setProperty("--light", "hsl(0 0%, 0%)");
+    rootSelector.style.setProperty("--semiDark", "hsl(0, 2%, 76%)");
+    rootSelector.style.setProperty("--semiLight", "hsl(0, 3%, 6%)");
+    mode = "light";
+  } 
+  else {
+    rootSelector.style.setProperty("--dark", "hsl(0 0%, 0%)");
+    rootSelector.style.setProperty("--light", "hsl(0 0%, 100%)");
+    rootSelector.style.setProperty("--semiDark", "hsl(0, 2%, 6%)");
+    rootSelector.style.setProperty("--semiLight", "hsl(0, 3%, 76%)");
+    mode = "dark";
+  }
+});
