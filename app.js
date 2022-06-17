c4a6de13298d5736f1afe776260b9b2181632ad7
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-06-17 18:27:45 +0500

    Updated

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const btnMode = document.querySelector(".btn-mode");
 const rootSelector = document.querySelector(":root");
 
-const dark = "hsl(0, 0%, 0%)";
-const light = "hsl(0, 0%, 100%)";
-const semiDark = "hsl(100, 0%, 0%)";
-const semiLight = "hsl(0, 0%, 100%)";
+const dark = "#b2e7e8";
+const light = "#304d63";
 
 let mode = "darkMode";
 btnMode.addEventListener("click", () => {
   if (mode === "darkMode") {
     rootSelector.style.setProperty("--dark", light);
     rootSelector.style.setProperty("--light", dark);
-    rootSelector.style.setProperty("--semiDark", semiLight);
-    rootSelector.style.setProperty("--semiLight", semiDark);
     mode = "lightMode";
   } 
   else {
     rootSelector.style.setProperty("--dark", dark);
     rootSelector.style.setProperty("--light", light);
-    rootSelector.style.setProperty("--semiDark", semiDark);
-    rootSelector.style.setProperty("--semiLight", semiLight);
     mode = "darkMode";
   }
 });
